47e9232448611820c2315896bf08b719143c62f8
MISHAL <160596015+Mishal55@users.noreply.github.com>
2024-10-19 03:19:17 -0700

    Add files via upload

---
+++ Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className= 'rounded-xl '>
+
+    <div className ="text-justify font-bold">
+<img src="https://wp.5balloons.info/wp-content/uploads/2018/02/footer-section-bootstrap-to-wordpress.png" 
+alt="footer" 
+className='h-full w-full'
+
+/>      
+    </div>
+    </div>
+  );
+}
---
+++ Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    
+    <div className ="h-[70px] shadow w-full bg-yellow-400 rounded-xl font-sans pt-5 pb-5">
+      <a className='font-normal text-justify text-gray-800 pt-7 pb-5 mx-5' href="#"> Home</a>
+
+</div>
+
+  );
+}
+
