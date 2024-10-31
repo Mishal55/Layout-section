8fe3d9677a3fb7f65c3234a28c1fb829c9ffd1a7
MISHAL <160596015+Mishal55@users.noreply.github.com>
2024-10-31 13:19:13 -0700

    Add files via upload

---
+++ app/components/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Image from 'next/image';
+export default function Hero() {
+  return (
+    <div className=""> 
+
+  <div className='rounded-lg  '>
+        <img
+         src="https://cdn.prod.website-files.com/600c68cb72173ece9c8958ed/637dc29d23239b4f6f305166_Hero%20Section.webp"
+         alt='hero pic'
+         className='h-full w-full rounded-xl'
+         />
+       </div>
+    </div>
+  );
+}
